aa189937470f288670dd2db626539473bf26eac4
Elonix1 <elias.agensa5@gmail.com>
2022-07-12 04:00:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main -get way
+ * Description: prints "\"programming is like building a multilingual puzzle"*
+ * Return: 0 always indicates success
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+
+	return (0);
+}
