f947742a9ae3be00d1c1e36def90cac5e0f0d379
Elonix1 <elias.agensa5@gmail.com>
2022-07-12 08:05:43 -0700

    improvement b code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,11 @@
-#include <stdio.h>
-
 #include <string.h>
-
-
-
 /**
- * main - gate way
- *Description:this code  prints "\"Programming is like building a multilingual puzzle"*
+ * main - entry point
+ *Description prints "\"Programming is like building a multilingual puzzle"*
  * Return: Always 0 (Success)
  */
-
-
-
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
