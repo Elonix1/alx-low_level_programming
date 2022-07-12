c8bac762ea9143c9ca73a5c61ad7b0be48054f37
Elonix1 <elias.agensa5@gmail.com>
2022-07-12 04:09:38 -0700

    an improvement to the code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
+
 #include <string.h>
 
+
+
 /**
- * main -get way
- * Description: prints "\"programming is like building a multilingual puzzle"*
- * Return: 0 always indicates success
- */
+ *
+ *  * main - gate way
+ *
+ *   * Description:this code  prints "\"Programming is like building a multilingual puzzle"*
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+
 
 int main(void)
+
 {
-	puts("\"programming is like building a multilingual puzzle");
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+
 
 	return (0);
+
 }
